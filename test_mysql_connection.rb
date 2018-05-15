4f172b69479b9d04e2ed689bb84333411597a1b3
dineshyadav009 <dinesh@joshtechnologygroup.com>
2018-05-15 13:51:36 +0530

    CE-3299 Updated test mysql script

--- test_mysql_connection.rb
+++ test_mysql_connection.rb
@@ -1,19 +1,20 @@
 require 'rubygems'
 require 'mysql2'
 
-(0..2).each do |index|
-	abort('error') if ARGV[index].nil? || ARGV[index].to_s.empty?
-end
-
 begin
-	if ARG[2].nil? || ARGV[2] == ''
-	  client = Mysql2::Client.new(host: ARGV[0], username: ARGV[1])
-	else
-	  client = Mysql2::Client.new(host: ARGV[0], username: ARGV[1], password: ARGV[2])
-	end
-	client.query('show global status')
-	client.close
-	puts('success')
+  options = { username: ARGV[0].to_s }
+  options[:password] = ARGV[1].to_s unless ARGV[1].to_s.empty?
+  if ARGV[3].nil? || ARGV[3].to_s.empty?
+    options[:socket] = ARGV[2].to_s
+  else
+    options[:host] = ARGV[2].to_s
+    options[:port] = ARGV[3].to_s
+  end
+
+  client = Mysql2::Client.new(options)
+  client.query('show global status')
+  client.close
+  puts('success')
 rescue StandardError
-	puts 'error'
+  puts 'error'
 end
